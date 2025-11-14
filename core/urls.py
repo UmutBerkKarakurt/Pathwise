8c0a20ce8e72bd1224a002566c84ee3a37df36c0
Acelya Yildiz <acelyayildiz@Acelya-MacBook-Pro.local>
2025-11-14 18:30:04 +0300

    umut için push

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import home
+from .views import ace
 
 urlpatterns = [
     path("", home, name="home"),
     path("home/", home, name="home_alt"),
+    path("ace/", ace, name="ace"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, "core/home.html")
+
+
+def ace(request):
+    return render(request, "core/ace.html")
