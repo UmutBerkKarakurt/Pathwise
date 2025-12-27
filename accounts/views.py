1aa009e940968f97687001066ec10376f34ed3bf
Umut Berk Karakurt <umutberkkarakurt222@gmail.com>
2025-12-27 16:14:18 +0300

    administration works

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,33 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
-def register(request):
+def register_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)          # kayýt olur olmaz login yap
-            return redirect("dashboard")  # dashboard url name'in bu
+            login(request, user)
+            return redirect("dashboard")
     else:
         form = UserCreationForm()
 
     return render(request, "accounts/register.html", {"form": form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("dashboard")
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "accounts/login.html", {"form": form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("core.urls")),  # <— ana sayfa core’dan
-    path("accounts/login/", auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
-    path("accounts/register/", include("accounts.urls")),
-
+    path("", include("core.urls")),
+    path("accounts/", include("accounts.urls")),  # âœ… login/register/logout buradan gelecek
 ]
